dd505d72f71ec3f44c7e780819690e7be1a6ff7e
Amy Charity Weng <amycweng@Amys-MacBook-Pro.local>
2022-10-03 15:40:51 -0400

    publishers

---
+++ playsStationers.py
@@ -0,0 +1,84 @@
+publishers = {
+    'Thomas Thorpe': 'Thomas Thorpe',
+    'Thomas Thorppe':'Thomas Thorpe',
+    'Nathanael Butter':'Nathaniel Butter',
+    'Nathaniell Butter':'Nathaniel Butter',
+    'Nathaniell Butter':'Nathaniel Butter',
+    'Nat Butter':'Nathaniel Butter',
+    'Nath Butter':'Nathaniel Butter',
+    'Nathnaiell sic Butter':'Nathaniel Butter',
+    'N Butter':'Nathaniel Butter',
+    'N athaniel Butter':'Nathaniel Butter',
+    'Na Butter':'Nathaniel Butter',
+    'Nathnaiell sic Butter':'Nathaniel Butter',
+    'N athaniel Butter':'Nathaniel Butter',
+    'Nathanaell Butter':'Nathaniel Butter',
+    'Andrevv Crooke':'Andrew Crooke',
+    'Andrew Crook':'Andrew Crooke',
+    'A Crooke':'Andrew Crooke',
+    'A Crook': "Andrew Crooke",
+    'Arthur Iohnson': 'Arthur Johnson',
+    'A rthur I ohnson':'Arthur Johnson',
+    'C Burby':'Cuthbert Burby',
+    'Cutbert Burbie':'Cuthbert Burby',
+    'Cuthbert Burbie':'Cuthbert Burby',
+    'Edward Blount':"Edward Blunt",
+    'Ed Blount':'Edward Blunt',
+    'E Marchant':'Edward Marchant',
+    'Edward VVhite':'Edward White',
+    'Edwarde White':'Edward White',
+    'Edwarde VVhite':'Edward White',
+    'F Coles':'Francis Coles',
+    'F Cole':'Francis Coles',
+    'Francis Coules':'Francis Coles',
+    'Edmond Weauer':'Edmond Weaver',
+    'Edvvard Langham':'Edward Langham',
+    'E Edgar':'Eleazar Edgar'
+}
+
+pubLocations = {
+    'Humphrey Moseley':["Sign of the Prince's Arms in St. Paul's Churchyard"],
+    'Humphrey Robinson':["Three Pigeons"],
+    'Samuel Speed':["Sign of the Rainbow between the two Temple Gates"],
+    "Samuel Rand":["Near Holborne Bridge"],
+    "Andrew Crooke":["Sign of the Green Dragon in St. Paul's Churchyard", "Castle gate in Dublin"],
+    "William Cooke":["Sign of the Green Dragon in St. Paul's Churchyard"],
+    "Arthur Johnson":["Sign of the White Horse in St. Paul's Churchyard"],
+    "Bartholomew Sutton":["Great North Door of St. Paul's Church"],
+    "William Barrenger":["Great North Door of St. Paul's Church"],
+    "Benjamin Fisher":["Sign of the Talbot without Aldersgate"],
+    "Cuthbert Burby":["Near the Royal Exchange","Middle Shop in the Poultry under St. Mildred's Church"],
+    "Edmond Matts":["Sign of the Hand and Plow in Fleet Street"],
+    "Edmond Weaver":["Great North Gate of St. Paul's Church"],
+    "Edward Blackmore":["Sign of the Angel in St. Paul's Churchyard"],
+    "Edward Marchant":["Against the Cross in St. Paul's Churchyard"],
+    "Edward White":["Little North Door of St. Paul's Churchyard at the Sign of the Gun"],
+    'Edward Langham':['Banbury'],
+    'Edward Brewster':["Sign of the Bible at the Great North Door of St. Paul's Church"],
+    "Francis Constable":["Sign of the Crane in St. Paul's Churchyard"],
+    "Francis Coles":["Upper End of the Old Baily Near Newgate"],
+    "Francis Faulkner":["Near St. Margaret's Hill in Southwark"],
+    'Francis Kirkman':["Sign of John Fletcher's Head over against the Angel Inn on the back side of St. Clements without Temple Bar"],
+    "George Norton":["Near Temple Bar"],
+    "George Vincent":["In Woodstreet"],
+    "Eleazar Edgar":["Sign of the Bull Head in St. Paul's Churchyard"],
+    "Samuel Macham":["Sign of the Bull Head in St. Paul's Churchyard"]
+
+
+}
+
+'''
+Unknowns:
+
+A I 
+E D and N E
+C G 
+
+Unknown locations:
+G Tomlinson
+Giles Calvert
+Edward Blunt 
+
+Weird syntax:
+By W White and another for T G ubbin and are to be sould by R i e E Marchant at the crosse in Pauls Church yard
+'''
